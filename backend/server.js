49d57b27f8cff32524e680dd4f04c461540a6aed
Sakshi Singh <sakshisingh67117@gmail.com>
2026-07-07 04:48:22 -0800

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Ghost Kitchen API is alive!');
@@ -25,5 +25,5 @@ app.use('/api/admin', adminRoutes);
 app.use('/api', metaRoutes);
 app.use('/api', recommendRoutes);
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
